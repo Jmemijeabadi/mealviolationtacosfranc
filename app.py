066c1f1f547303460cc47b4b7bb0238dfbfa77b3
Jmemijeabadi <Jmemije@grupoabadi.mx>
2025-08-05 23:00:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -50,11 +50,9 @@ def process_csv_toast(file, progress_bar=None):
     for (name, date), group in grouped:
         total_hours = group["Total Hours"].sum()
 
-        # Criterio de violación:
-        # Si 'Break Duration' es "MISSED" o su valor es mayor a 0.50, es una violación
-        missed_break = group[(group["Break Duration"] == "MISSED") | (group["Break Duration"] > 0.50)]
+        # Criterio 1: Si la columna 'Break Duration' tiene el valor "MISSED" o su duración es mayor a 0.50, es una violación
+        missed_break = group[(group["Break Duration"].isna()) | (group["Break Duration"] > 0.50)]
 
-        # Si hay violaciones de comida (por "MISSED" o duración > 0.50), las agregamos a la lista de violaciones
         if not missed_break.empty:  # Si hay una violación de comida
             violations.append({
                 "Nombre": name,
